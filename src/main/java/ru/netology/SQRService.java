aa56afd531eb06a31d9739d6aed1c1031e569b25
Роман Подгорный <110032141+ripodgor@users.noreply.github.com>
2022-09-26 17:44:48 +0300

    create files

---
+++ src/main/java/ru/netology/SQRService.java
@@ -0,0 +1,16 @@
+package ru.netology;
+
+public class SQRService {
+
+    public int countSquares(int minimum, int maximum) {
+        int count = 0;
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= minimum && i * i <= maximum) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
